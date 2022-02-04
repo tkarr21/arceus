485faef1ab4ca959a7a3dfb5556de681acb7ccec
Tyler Karren <tkarren22@gmail.com>
2022-02-04 13:12:13 -0700

    rough outline of creative system

---
+++ createPoke.py
@@ -0,0 +1,22 @@
+'''
+input: hp (int), att (int), sp att (int), def (int), sp def(int), speed(int), element TYPE
+
+step 1:
+grab k nearest (euclidean distance) pokemon from dataframe -> [pokemonIDs]
+
+step 2:
+Decide on a shape (grammar of construction) -> mode shape of [pokemonIDs]??
+["ball", "squiggle", "fish", "arms", "blob", "upright", "legs", "quadruped", "wings", "tentacles", "heads", "humanoid", "bug-wings", "armor"]
+
+based off shape grab required body parts from assets of [pokemonIDs] -> random??
+
+layer body parts according to shape
+
+choose and apply color scheme
+
+
+
+
+
+
+'''
